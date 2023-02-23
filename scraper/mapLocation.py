4f8199b4cfdf50e4f58a8df9a805e983ac9ea3c4
lucakao <luca.kao@simpnic.com>
2023-02-23 18:40:35 +0800

    [Fix] fix loop map location

--- scraper/mapLocation.py
+++ scraper/mapLocation.py
@@ -38,27 +38,57 @@ browser.get("https://www.google.com/maps/")
 
 
 
-def search_place(place_name):
-    print("init ")
+def search_place_data(place_name):
+    print("init data")
     Place = browser.find_element(By.CLASS_NAME,'searchboxinput')
     print(place_name)
+    Place.send_keys("")
     Place.send_keys(place_name)
     submit = browser.find_element(By.ID,"searchbox-searchbutton")
     print(submit.text)
     submit.click()
-    time.sleep(15)
+    time.sleep(5)
 
     # 有資料
     place_list = browser.find_elements(By.CLASS_NAME, 'hfpxzc')
     print(place_list)
     for item in place_list:
-        item.find_element()
-        print(item)
+        # item.find_element()
+        print(item.text)
     # 找不到資料 
     # text = browser.find_element(By.CLASS_NAME, 'Q2vNVc')
     # print(text.text)
-    # time.sleep(15)
+    time.sleep(3)
+    clear = browser.find_element(By.XPATH,'//*[@id="searchbox"]/div[3]/button/div')
+    print(clear.text)
+    clear.click()
+    time.sleep(3)
+
+
     print('search key word finish')
+
+
+def search_place_no_data(place_name):
+    print("init no data")
+    Place = browser.find_element(By.CLASS_NAME,'searchboxinput')
+    print(place_name)
+    Place.send_keys("")
+    Place.send_keys(place_name)
+    submit = browser.find_element(By.ID,"searchbox-searchbutton")
+    print(submit.text)
+    submit.click()
+    time.sleep(3)
+
+    # 找不到資料 
+    text = browser.find_element(By.CLASS_NAME, 'Q2vNVc')
+    print(text.text)
+    time.sleep(5)
+    clear = browser.find_element(By.XPATH,'//*[@id="searchbox"]/div[3]/button/div')
+    print(clear.text)
+    clear.click()
+    time.sleep(3)
+    print('search key word finish')
+
 
 if __name__ == "__main__":
     print("map location start")
@@ -67,13 +97,30 @@ if __name__ == "__main__":
     with open(fn) as csvFile : 
         csvReader = csv.reader(csvFile) 
         listReport = list(csvReader) 
-        
-    search_place(listReport[1][1])
+    # for i in range(11):
+    #     if(i!=0):
+    #         search_place_data(listReport[i][1])
+
+    # try:
+    #     search_place_data(listReport[1613][1])
+    # except Exception as e: 
+    #     search_place_no_data(listReport[1613][1])
+
+    
+    for i in range(30):
+        print("index" , i)
+        if i!=0:
+            try:
+                print(" ok ",listReport[i][0],listReport[i][1])
+                search_place_data(listReport[i][1])
+                # continue
+            except Exception as NoSuchElementException: 
+                print("error",listReport[i][0],listReport[i][1])
+                search_place_no_data(listReport[i][1])
+                # continue
+    
     time.sleep(15)
     
-    # for i in range(1):
-    #     if i!=0:
-    #         print(listReport[i][0],listReport[i][1])
             
     # 2. check loc is exist
     
