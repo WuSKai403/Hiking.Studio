43ceea04a7eae3f3df4690aab77ecfd8dddb691e
lucakao <luca.kao@simpnic.com>
2023-02-23 14:11:03 +0800

    [Fix] add write file func to hiking index func

--- scraper/hikingIndex.py
+++ scraper/hikingIndex.py
@@ -1,13 +1,8 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 from env_crawler import hiking_index_url,driver_path,options
 import requests
 from models import Attraction
-
+import csv
 
 # browser = webdriver.Chrome(chrome_options=options ,executable_path=driver_path)
 # browser = webdriver.Chrome(executable_path='./chromedriver')
@@ -19,10 +14,22 @@ def check_req_status(id):
         print(r)
         return False
     else:
+        get_data(r,id)
         return True
+
+def get_data(r,id):
+    soup = BeautifulSoup(r.text, "html5lib")
+    title = soup.find(class_="text-3xl").getText()
+    text = soup.find(class_="leading-relaxed").getText()
+    loc = soup.find(class_="flex-1 p-4").getText()
+    writer.writerow([id,title,text, loc,hiking_index_url+str(id)])
+    print(id,title , loc)
 
 
 if __name__ == "__main__":
+    with open('Location.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['id','title', 'text','loc','hiking_url'])
 
     for id in range(1750):
         if check_req_status(id)==True:
